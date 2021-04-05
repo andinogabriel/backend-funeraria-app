afede8b5a5efa0fa2ca10a26a6f75f34b475f3a2
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-05 20:05:51 -0300

    DS0010 - Agrego la entidad direccion.

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/entities/AddressEntity.java
@@ -0,0 +1,35 @@
+package disenodesistemas.backendfunerariaapp.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "addresses")
+@Getter @Setter
+public class AddressEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, length = 90)
+    private String streetName;
+
+    private Integer blockStreet; //Altura de la calle
+
+    @Column(length = 90)
+    private String apartment;
+
+    @Column(length = 90)
+    private String flat; //Piso del departamento
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private CityEntity city;
+
+
+}
