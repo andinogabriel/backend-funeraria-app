76093cbee3cff12066e8e3c844ab18455d315baa
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-05 20:04:33 -0300

    DS0008 - Agrego la entidad provincia.

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/entities/ProvinceEntity.java
@@ -0,0 +1,28 @@
+package disenodesistemas.backendfunerariaapp.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "provinces")
+@Getter @Setter
+public class ProvinceEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, length = 90)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "province")
+    private List<CityEntity> cities = new ArrayList<>();
+
+
+}
