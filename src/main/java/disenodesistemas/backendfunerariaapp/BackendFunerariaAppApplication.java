fe7183d6b3ee05e66865c984b7716057afd9aa20
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-05 18:32:34 -0300

    first commit

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/BackendFunerariaAppApplication.java
@@ -0,0 +1,13 @@
+package disenodesistemas.backendfunerariaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFunerariaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFunerariaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/disenodesistemas/backendfunerariaapp/BackendFunerariaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package disenodesistemas.backendfunerariaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFunerariaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
