59e0fb618c1f38dcd6de296ff413074f5da60d90
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-05 19:32:25 -0300

    DS0007 - Agrego la entidad numero de telefono.

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/entities/MobileNumberEntity.java
@@ -0,0 +1,27 @@
+package disenodesistemas.backendfunerariaapp.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "mobileNumbers")
+@Getter @Setter
+public class MobileNumberEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, length = 75)
+    private String mobilNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity userNumber;
+
+
+}
