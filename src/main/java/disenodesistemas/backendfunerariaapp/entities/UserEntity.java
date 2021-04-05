d93ad2ccd7f584cb06e176b51edec3895e6be61a
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-05 19:05:52 -0300

    DS0002 - Agrego la entidad Usuario, todavia faltan cosas para agregar

--- src/main/java/disenodesistemas/backendfunerariaapp/entities/UserEntity.java
+++ src/main/java/disenodesistemas/backendfunerariaapp/entities/UserEntity.java
@@ -1,2 +1,39 @@
-package disenodesistemas.backendfunerariaapp.entities;public class UserEntity {
+package disenodesistemas.backendfunerariaapp.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "users")
+@Table(indexes = { @Index(columnList = "userId", name = "index_userid", unique = true), @Index(columnList = "email", name = "index_email", unique = true) })
+@Getter @Setter
+public class UserEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false, length = 75)
+    private String firstName;
+
+    @Column(nullable = false, length = 75)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Integer dni;
+
+    @Column(nullable = false, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String encryptedPassword;
+
+
 }
