e45b6f549b9a79fff01795962125d774821e124b
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-10 12:43:19 -0300

    DS 0018 - Agrego las funcionalidades de seguridad de la app, AppProperties es para obtener el token secreto, AuthenticationFilter para verificar cuando el usuario loguea, AuthorizationFilter verifica si el usuario esta autorizado para realizar acciones dentro de la app si tiene el token etc. SecurityConstants son constantes de seguridad y WebSecurity esta el encriptador de password, el cors y los permisos sin token a las distintas endpoints.

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/security/AppProperties.java
@@ -0,0 +1,18 @@
+package disenodesistemas.backendfunerariaapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+//Esta clase es para poder leer application.properties
+@Component
+public class AppProperties {
+
+    @Autowired
+    private Environment env;
+
+    public String getTokenSecret() {
+        return env.getProperty("tokenSecret");
+    }
+
+}
