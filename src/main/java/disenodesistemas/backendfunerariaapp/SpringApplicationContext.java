8a457effcdb1f86e5bfef37e00e5255a4f2f65e6
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-10 14:23:38 -0300

    DS 0024 - Creamos una clase para que sea global y otras clases puedan usar bean y autoWired

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/SpringApplicationContext.java
@@ -0,0 +1,21 @@
+package disenodesistemas.backendfunerariaapp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class SpringApplicationContext implements ApplicationContextAware {
+
+    private static ApplicationContext CONTEXT;
+
+    //Ponemos en nuestra constante el app context
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        CONTEXT = applicationContext;
+    }
+
+    //Hacemos esto porque en algunas clases no podemos usar beans ni autoWired
+    public static Object getBean(String beanName ) {
+        return CONTEXT.getBean(beanName);
+    }
+}
