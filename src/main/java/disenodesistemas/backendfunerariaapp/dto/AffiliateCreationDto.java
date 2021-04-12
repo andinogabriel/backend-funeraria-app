61455901ad5eec8cc8104842e9d175dc079aa342
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-12 20:49:32 -0300

    DS 0029 - Agrego el dto de creacion de afiliado.

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/dto/AffiliateCreationDto.java
@@ -0,0 +1,24 @@
+package disenodesistemas.backendfunerariaapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter @Setter
+public class AffiliateCreationDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String firstName;
+    private String lastName;
+    private Integer dni;
+    private Date birthDate;
+    private Date startDate;
+    private long affiliateGender;
+    private String userEmail;
+    private long affiliateRelationship;
+
+
+}
