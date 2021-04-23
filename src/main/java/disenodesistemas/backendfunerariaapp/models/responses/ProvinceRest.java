ea0efc98f21b8640698fc113dc0411634af97fa7
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-23 20:56:06 -0300

    DS 0049 - Agrego la response de provincia

---
+++ src/main/java/disenodesistemas/backendfunerariaapp/models/responses/ProvinceRest.java
@@ -0,0 +1,13 @@
+package disenodesistemas.backendfunerariaapp.models.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ProvinceRest {
+
+    private long id;
+    private String code31662;
+    private String name;
+
+}
